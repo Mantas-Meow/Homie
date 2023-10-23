b7ee46735d35533cdc3b67238ef673dc64400167
Gabu <gabijaburok@gmail.com>
2023-10-23 19:04:16 +0300

    Created db context, the startup program for the data project and created an initial migration

--- Homie.Data/Program.cs
+++ Homie.Data/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Homie.Data
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            CreateHostBuilder(args).Build().Run();
+ 
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+    }
+}
